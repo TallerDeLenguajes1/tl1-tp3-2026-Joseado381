1da2e6f4f01519f0d4caf661abb9fca86659af4d
Joseado381 <josealonzo.jaa@gmail.com>
2026-04-16 18:28:17 -0300

    commit a ver si me deja pushear

---
+++ ArreglodeNombres.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+#define N 50
+
+int main() {
+    char Buff[N];
+    char *nombres[N];
+
+    printf("Ingrese el nombre de 5 personas:\n");
+
+    for (int i = 0; i < 5; i++)
+    {
+        
+        /* code */
+    }
+    
+
+    
+    getchar();
+    return 0;
+}
